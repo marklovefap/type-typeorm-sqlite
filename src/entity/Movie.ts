cd11279e282a24a2bddff818924f830640ca6764
mark <hok31656@gmail.com>
2022-11-22 03:18:40 +0700

    final

---
+++ src/entity/Movie.ts
@@ -0,0 +1,20 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+@ObjectType()
+@Entity()
+export class Movie extends BaseEntity {
+    
+    @Field(() => Int)
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Field()
+    @Column()
+    title: string
+
+    @Field(() => Int)
+    @Column('int', { default: 60 })
+    minutes: number
+
+}
